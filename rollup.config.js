3cc0788fc801147323e1c0d1048445db4846f42a
Valentin Zhukovsky <valentin.zhukovsky@kandasoft.com>
2019-11-26 14:27:20 +0300

    Changed library settings in package.json
    > change library name
    > change entry point

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,16 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default [
 	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: 'src/index.js',
 		output: {
-			name: 'howLongUntilLunch',
-			file: pkg.browser,
-			format: 'umd'
-		},
+      format: 'cjs',
+      file: 'dist/vast-client-to-xml.js'
+    },
 		plugins: [
 			resolve(), // so Rollup can find `ms`
 			commonjs(), // so Rollup can convert `ms` to an ES module
@@ -25,10 +24,10 @@ export default [
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
 	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
+	// an array for the `output` option, where we can specify
 	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
+		input: 'src/index.js',
 		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
